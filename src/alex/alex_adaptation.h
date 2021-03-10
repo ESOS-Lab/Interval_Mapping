6218da5a2c85bb442ce13eab15831a732137766c
Minsu Jang <nobleminsu@gmail.com>
2021-03-10 13:06:12 +0900

    Fix 64-bit popcount fix

--- src/alex/alex_adaptation.h
+++ src/alex/alex_adaptation.h
@@ -8,7 +8,7 @@
 #ifndef SRC_ALEX_ALEX_ADAPTATION_H_
 #define SRC_ALEX_ALEX_ADAPTATION_H_
 
-#define _mm_popcnt_u64 __builtin_popcountl
+#define _mm_popcnt_u64 __builtin_popcountll
 #define _lzcnt_u64 __builtin_clzl
 #define _tzcnt_u64 __builtin_ctzl
 
