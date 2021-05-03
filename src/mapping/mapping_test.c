394f621eb77d12c9a9d66d5091722dd5c6e6f1d9
Minsu Jang <nobleminsu@gmail.com>
2021-05-03 16:40:22 +0900

    Implement FTable mapping test

--- src/mapping/mapping_test.c
+++ src/mapping/mapping_test.c
@@ -18,9 +18,16 @@ FTable testHotFTables[FTABLE_DEFAULT_TABLE_NUM];
 void test_fhm() {}
 void test_hotmap() {}
 void test_ftable() {
-    xil_printf("curcur=%d\n", testCurMaxFtableIdx);
-    FTable* ftable = ftable_create_table(0, testHotFTables, &testCurMaxFtableIdx,
-                                         FTABLE_DEFAULT_TABLE_NUM);
+    xil_printf("Starting ftable test...\n");
+    FTable* ftable = ftable_create_table(
+        0, testHotFTables, &testCurMaxFtableIdx, FTABLE_DEFAULT_TABLE_NUM);
     ftable_insert(ftable, 0, 1);
     assert(ftable_get(ftable, 0) == 1);
+
+    int i;
+    for (i = 0; i < FTABLE_DEFAULT_CAPACITY * 10; i += 10000) {
+        ftable_insert(ftable, i, i + 100);
+        assert(ftable_get(ftable, i) == i + 100);
+    }
+    xil_printf("ftable test ok.\n");
 }
